d667bd6befefd6bcf4c9c03096051b92e1cf2ada
Asadullah404 <151784066+Asadullah404@users.noreply.github.com>
2024-08-07 00:06:57 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+<!-- firebase-config.js -->
+
+  // Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCRRGLY5wimSd9GdJt3Np-BFI9tFAFjvnU",
+    authDomain: "conversion-6394a.firebaseapp.com",
+    projectId: "conversion-6394a",
+    storageBucket: "conversion-6394a.appspot.com",
+    messagingSenderId: "1053184555205",
+    appId: "1:1053184555205:web:f85d614a068cfccde218ac"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var firestore = firebase.firestore();
+
